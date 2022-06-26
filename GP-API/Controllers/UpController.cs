fb6e08f08fef8cd47f05bd75fbdbdede5090dee3
Rohan Singh <rohan.s@deltax.com>
2022-06-26 22:57:44 +0530

    initate a project, add up controller for launch url add git ignore file

---
+++ GP-API/Controllers/UpController.cs
@@ -0,0 +1,15 @@
+﻿namespace GP_API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("up")]
+    [Controller]
+    public class UpController : Controller
+    {
+        [HttpGet]
+        public IActionResult UpStatus()
+        {
+            return Ok(new { data = "Gaming Profile is up and ready to go !!" });
+        }
+    }
+}
